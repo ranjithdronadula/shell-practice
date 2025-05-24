606e49eb4df9fd88fa4e01bc9bc900d273dc9ee6
ranjithdronadula <dronadularanjith@gmail.com>
2025-05-24 14:46:44 +0530

    hello iam new to git

---
+++ 05-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSION1=$1
+PERSION2=$2
+
+echo "$PERSION1:: Hey $PERSION2, How are you?"
+echo "$PERSION2:: Hello $PERSION1, I am fine. How are you doing"
+echo "$PERSION1: I am fine too. What's up?"
+echo "$PERSION2:: Nohting, just going to Mars now, will you come?"
+echo "$PERSION1:: Sorry, you carry on! I will come once you come back"
