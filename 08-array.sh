de6a6af931a02e61b4352b0d719c7b77340290b1
ranjithdronadula <dronadularanjith@gmail.com>
2025-05-24 15:46:18 +0530

    hello iam new to git

--- 08-array.sh
+++ 08-array.sh
@@ -2,4 +2,9 @@
 
 MOVIES=("court"  "Hit"  "Thandel" "Khaleja")
 
+
 echo "first movie = ${MOVIES[0]}"
+echo "first movie = ${MOVIES[1]}"
+echo "first movie = ${MOVIES[2]}"
+echo "first movie = ${MOVIES[7]}"
+echo "first movie = ${MOVIES[@]}"
