31af348dc1b0e5ca103b8f96202c4518fb75c5f5
ranjithdronadula <dronadularanjith@gmail.com>
2025-05-24 18:31:01 +0530

    special vars

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -7,9 +7,9 @@ NUMBER=$1
 # -eq --> equal
 # -ne --> not equal
 
-if [ $NUMBER1 $NUMBER2 -lt 10 ]
+if [ $NUMBER -lt 10 ]
 then
-    echo "Given number  $NUMBER1 $NUMBER2  is less than 10"
+    echo "Given number  $NUMBER is less than 10"
 else
-    echo "Given number $NUMBER1 $NUMBER2 is not less than 10"
+    echo "Given number $NUMBER is not less than 10"
 fi
