0c5f45bf7938c9c9f960e89590c275a422a3c0d7
ranjithdronadula <dronadularanjith@gmail.com>
2025-05-26 19:02:46 +0530

    colors

--- 13-colors.sh
+++ 13-colors.sh
@@ -14,15 +14,15 @@ else
     echo "You are running with root access"
 fi
 
-# VALIDATE(){
-#     if [ $1 -eq 0 ]
-#     then
-#         echo -e "Installing $2 is ... $G SUCCESS $N"
-#     else
-#         echo -e "Installing $2 is ... $R FAILURE $N"
-#         exit 1
-#     fi
-# }
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "Installing $1 is ... $G SUCCESS $N"
+    else
+        echo -e "Installing $2 is ... $R FAILURE $N"
+        exit 1
+    fi
+}
 
 dnf list installed mysql
 if [ $? -ne 0 ]
@@ -34,22 +34,22 @@ else
     echo -e "Nothing to do MySQL... $Y already installed $N"
 fi
 
-# dnf list installed python3
-# if [ $? -ne 0 ]
-# then
-#     echo "python3 is not installed... going to install it"
-#     dnf install python3 -y
-#     VALIDATE $? "python3"
-# else
-#     echo -e "Nothing to do python... $Y already installed $N"
-# fi
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed... going to install it"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo -e "Nothing to do python... $Y already installed $N"
+fi
 
-# dnf list installed nginx
-# if [ $? -ne 0 ]
-# then
-#     echo "nginx is not installed... going to install it"
-#     dnf install nginx -y
-#     VALIDATE $? "nginx"
-# else
-#     echo -e "Nothing to do nginx... $Y already installed $N"
-# fi
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo "nginx is not installed... going to install it"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo -e "Nothing to do nginx... $Y already installed $N"
+fi
