608de1297b0a24e2103b89228a9317d2baffc5c7
ranjithdronadula <dronadularanjith@gmail.com>
2025-05-26 18:42:28 +0530

    colors

--- 13-colors.sh
+++ 13-colors.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR:: Please run this script with root access $N"
+    echo -e "$G ERROR:: Please run this script with root access $N"
     exit 1 #give other than 0 upto 127
 else
     echo "You are running with root access"
