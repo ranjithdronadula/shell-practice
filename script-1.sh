6aa293adef0c65de98491ce5ad846ecdaa03d74b
ranjithdronadula <dronadularanjith@gmail.com>
2025-06-23 17:00:02 +0530

    call one script to onther script

--- script-1.sh
+++ script-1.sh
@@ -5,7 +5,7 @@ GREATING="Calling one script to onther script"
 echo -e "$GREATING"
 
 echo "PID of SCRIPT-1: $$"
+                            # BY USING WHENEVER WE USING SAME SCRIPT IN DIFFRENT SCRIPTS IN THAT PLACE WE USE THIS  
+# ./script2.sh              #PID DIFFRENT                                                 2 METHODS
 
-# ./script2.sh
-
-source ./script-2.sh
+source ./script-2.sh        #PID SAME
