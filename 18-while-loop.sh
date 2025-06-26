d64aef1d278d3710c055f850a6ca1dd450a0d148
ranjithdronadula <dronadularanjith@gmail.com>
2025-06-26 18:47:20 +0530

    while loop

--- 18-while-loop.sh
+++ 18-while-loop.sh
@@ -11,4 +11,9 @@
 while IFS= read -r line
 do
     echo $line
-done < 14-forloop.sh
+done < set -e    # This command ensures that the script will exit immediately if any command fails 
+                        # (returns a non-zero exit status)
+
+echo "Hi, Good Morning"                 # THIS STATEMENT  IS PRINT
+echoooooo "Hello, this will be error"   # THIS STATEMENT IS NOT BE PRINT BEUS COMMAND ERROR
+echo "Hello, Morning" 
