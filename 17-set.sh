e2212e36a04dd70dde354e32563ce34e8c7aa540
ranjithdronadula <dronadularanjith@gmail.com>
2025-06-26 18:20:02 +0530

    set command

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set -e
+set -e    # This command ensures that the script will exit immediately if any command fails 
+                        # (returns a non-zero exit status)
 
 echooooooooooo "Hi, Good Morning"
 echo "Hi, Good Morning"
