3519c319e654f76294548dee77d10d85109d3bff
ranjithdronadula <dronadularanjith@gmail.com>
2025-06-26 18:50:00 +0530

    while loop

--- 18-while-loop.sh
+++ 18-while-loop.sh
@@ -8,12 +8,7 @@
 #    a=`expr $a + 1`
 # done
 
-while IFS= read -r line
+while IFS= read -r line   
 do
     echo $line
-done < set -e    # This command ensures that the script will exit immediately if any command fails 
-                        # (returns a non-zero exit status)
-
-echo "Hi, Good Morning"                 # THIS STATEMENT  IS PRINT
-echoooooo "Hello, this will be error"   # THIS STATEMENT IS NOT BE PRINT BEUS COMMAND ERROR
-echo "Hello, Morning" 
+done < 17-set-sh   # SCRIPT 17-set-sh. entire content will be displyed
