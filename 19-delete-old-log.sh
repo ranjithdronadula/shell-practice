3def210d6b92dc25436c4f54cab70738248a373d
ranjithdronadula <dronadularanjith@gmail.com>
2025-06-26 19:02:27 +0530

    delete-old-log-files

--- 19-delete-old-log.sh
+++ 19-delete-old-log.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SOURCE_DIR=/home/ec2-user/app-logs
+SOURCE_DIR=/home/ec2-user/app-log
 
 mkdir -p $LOGS_FOLDER
 
